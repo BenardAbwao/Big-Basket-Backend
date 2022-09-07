ea0d9df0a4caf710b0a2145e845159433b5dea34
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-09-07 14:56:54 +0300

    create authors migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_114437) do
+ActiveRecord::Schema.define(version: 2022_09_07_115524) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.text "content"
